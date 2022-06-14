b54f54978f5f7e738fd0aa6616b4193957b017ca
minh-turing <minh.nguyen@turing.com>
2022-06-14 17:19:21 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="test_python_package",
+    version="0.0.1",
+    author="M",
+    author_email="m@gmail.com",
+    description="A function that returns 'world'",
+    py_modules=['hello'],
+    scripts=['hello.py']
+)
